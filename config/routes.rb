7ac4ef2a5603179d08cc0aebe5150e16f9e0f1e6
Rachel <rachelmramirez@gmail.com>
2015-06-21 00:22:15 -0400

    so many seed problems

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,5 @@ Rails.application.routes.draw do
 
   get 'welcome/about'
 
-  get 'welcome/contact'
-
 root to: 'welcome#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,9 @@
-require 'faker'
-
-# Create Posts
-50.times do
-  Post.create!(
-    title: Faker::Lorem.sentence,
-    body:  Faker::Lorem.paragraph
-  )
-end
-posts = Post.all
-
-Post.find_or_create_by!(
+Post.find_or_create_by(
   title: "When Harry Met Sally",
   body: "This is my favorite movie."
 )
 
-# Create Comments
-100.times do
-  Comment.create!(
-    post: posts.sample,
-    body: Faker::Lorem.paragraph
-  )
-end
-
-Comment.find_or_create_by!(
-  post: posts.last,
+Comment.find_or_create_by(
   body: "Mine too!"
 )
 
