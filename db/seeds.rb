0c7fba3552567431893cd992a5a41eb41f01ac4f
Rachel <rachelmramirez@gmail.com>
2015-06-24 20:04:43 -0400

    fixed SENDGRID set-up, sign-in, and completed Associations checkpoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,52 @@
 require 'faker'
 
+# Create Users
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
+# Note by calling 'User.new' instead of 'create',
+# we create an instance of User which isn't immediately saved to the database.
+
+# The 'skip_confirmation!' method sets the 'confirmed_at' attribute
+# to avoid triggering a confirmation email when the User is saved.
+
+# The 'save' method then saves this User to the database.
+
 # Create Posts
 50.times do
   Post.create!(
+    user:  users.sample,
     title: Faker::Lorem.sentence,
     body:  Faker::Lorem.paragraph
   )
 end
 posts = Post.all
 
-Post.find_or_create_by!(
-  title: "When Harry Met Sally",
-  body: "This is my favorite movie."
-)
-
 # Create Comments
 100.times do
   Comment.create!(
+    # user: users.sample,   # we have not yet associated Users with Comments
     post: posts.sample,
     body: Faker::Lorem.paragraph
   )
 end
 
-Comment.find_or_create_by!(
-  post: posts.last,
-  body: "Mine too!"
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes!(
+  email: 'rachelmramirez@gmail.com',
+  password: 'helloworld'
 )
 
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
