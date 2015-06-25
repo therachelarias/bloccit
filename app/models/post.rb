ebfbadc11b8c8756ef9e7c2723cca64ca98e2d52
Rachel <rachelmramirez@gmail.com>
2015-06-24 21:07:28 -0400

    order assignment complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,7 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
 
-  default_scope { order('created_at DESC') }
+  default_scope {order('created_at DESC')}
+  scope :ordered_by_title, {order('title ASC')}
+  scope :ordered_by_reverse_created_at, {reverse_order('title ASC, created_at')}
 end
