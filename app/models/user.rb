a05756326a01d56f368e35acc1b95a5c6af98092
Rachel <rachelmramirez@gmail.com>
2015-07-03 17:06:34 -0400

    checkpoint 44 in progress

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   has_many :posts
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  mount_uploader :avatar, AvatarUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150628233031) do
+ActiveRecord::Schema.define(version: 20150703195524) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -42,6 +42,15 @@ ActiveRecord::Schema.define(version: 20150628233031) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "summaries", force: :cascade do |t|
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "post_id"
+  end
+
+  add_index "summaries", ["post_id"], name: "index_summaries_on_post_id"
+
   create_table "topics", force: :cascade do |t|
     t.string   "name"
     t.boolean  "public",      default: true
@@ -69,6 +78,7 @@ ActiveRecord::Schema.define(version: 20150628233031) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "role"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
