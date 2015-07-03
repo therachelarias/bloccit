a1ad18a8ec7f32f9ce0fa9584e0b198115e04005
Rachel <rachelmramirez@gmail.com>
2015-07-03 15:43:21 -0400

    checkpoint 43 in progress

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,14 @@ module ApplicationHelper
     if errors.any?
       content_tag :div, capture(&block), class: 'form-group has-error'
     else
-      content_tag :div, capture (&block), class: 'form-group'
+      content_tag :div, capture(&block), class: 'form-group'
     end
+  end
+
+  def markdown_to_html(markdown)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render markdown).html_safe
   end
 end
