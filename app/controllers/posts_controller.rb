b65256cb7f0af89f0d3b9cb86290d779bc2f5226
Rachel <rachelmramirez@gmail.com>
2015-07-04 19:33:36 -0400

    posts markdown assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,7 @@ class PostsController < ApplicationController
 
     if @post.save
       flash[:notice] = "Post was saved."
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :new
