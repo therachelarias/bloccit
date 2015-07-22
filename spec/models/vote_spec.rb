222f8d40187e56d5fb48c2c3a26d83714c38c58c
Rachel <rachelmramirez@gmail.com>
2015-07-21 21:16:14 -0400

    updated rspec assignment

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -2,10 +2,11 @@ describe Vote do
   describe "validations" do
     describe  "value validation" do
       it "only allows -1 or 1 as values" do
-        if Vote.new(value: 1 || -1)
-        then 
+        if 
+          Vote.new(value: 1 || -1)
+        then
         expect(@vote.valid?).to eq(true)
-      else
+      end
         expect(@vote.valid?).to eq(false)
       end
     end
