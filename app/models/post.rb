b9e8012669cad3f30c7095dc128944d8775c3da2
Rachel <rachelmramirez@gmail.com>
2015-07-24 20:56:45 -0400

    complete voting assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,10 +8,12 @@ class Post < ActiveRecord::Base
 
   validates :title, length: {minimum: 5}, presence: true
   validates :body, length: {minimum: 20}, presence:true
-  # validates :topic, presence: true
-  # validates :user, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
 
   mount_uploader :image, ImageUploader
+
+  after_create :create_vote
 
   def up_votes
     votes.where(value: 1).count
@@ -30,5 +32,11 @@ class Post < ActiveRecord::Base
     new_rank = points + age_in_days
 
     update_attribute(:rank, new_rank)
+  end
+
+  private
+
+  def create_vote
+    user.votes.create(value: 1,post: self)
   end
 end
