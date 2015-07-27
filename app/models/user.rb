d3156b2861f87e93283361fddc6b4f5b0b0b2687
Rachel <rachelmramirez@gmail.com>
2015-07-27 17:02:44 -0400

    favoriting checkpoint in progress

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   has_many :comments
   has_many :votes, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   mount_uploader :avatar, AvatarUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
@@ -12,5 +13,9 @@ class User < ActiveRecord::Base
 
   def moderator?
     role == 'moderator'
+  end
+
+  def favorited(post)
+    favorites.where(post_id: post.id).first
   end
 end
---
+++ db/migrate/20150726220422_add_email_permision_to_users.rb
@@ -0,0 +1,5 @@
+class AddEmailPermisionToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :email_favorites, :boolean, default: true
+  end
+end
