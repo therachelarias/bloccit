f70ba7e87b1ba841c2d0e0aec2cb3ccc507979aa
Rachel <rachelmramirez@gmail.com>
2015-07-28 20:03:25 -0400

    favoriting assignment dunzo

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,19 @@ describe User do
   include TestFactories
 
   describe "#favorited(post)" do
-    xit "returns `nil` if user has not favorited the post" do
+    
+    before do
+      @user = authenticated_user
+      @post = associated_post
     end
 
-    xit "returns the appropriate favorite if it exists" do
+    it "returns `nil` if user has not favorited the post" do
+      expect(@user.favorited(@post)).to eq(nil)
+    end
+
+    it "returns the appropriate favorite if it exists" do
+      favorite = @user.favorites.where(post: @post).create
+      expect(@user.favorited(@post)).to eq(favorite)
     end
   end
 end
