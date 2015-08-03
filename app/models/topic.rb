80263dac38731deb5d636a8fbda58f0a92a24b5b
Rachel <rachelmramirez@gmail.com>
2015-08-03 19:08:12 -0400

    private topics assignment complete

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,6 @@ class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
 
   scope :visible_to, -> (user) {user ? all : where(public: true)}
+  scope :publicly_viewable, -> (user) {user ? true : where(public: true)}
+  scope :privately_viewable, -> (user) {user ? false : where(public: false)}
 end
