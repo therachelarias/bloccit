72c4c6978f186bdac299210a166780347cfd1791
Rachel <rachelmramirez@gmail.com>
2015-08-08 18:42:39 -0400

    public profiles assignment complete

--- spec/features/profile_spec.rb
+++ spec/features/profile_spec.rb
@@ -24,4 +24,21 @@ describe "Visiting profiles" do
     end
 
   end
+
+  describe "user visiting own profile" do
+    
+    before do
+      user = authenticated_user
+      login_as(user, :scope => :user)
+    end
+
+    it "shows profile" do
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+
+      expect(page).to have_content(@user.name)
+      expect(page).to have_content(@post.title)
+      expect(page).to have_content(@comment.body)
+    end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,8 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rails'
 require 'capybara/rspec'
+include Warden::Test::Helpers
+Warden.test_mode!
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
